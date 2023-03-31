c43a14f9ada574c9a91464d2d3388e5689496060
CheddarAnalyst <xan404cheddar@gmail.com>
2023-03-31 16:57:54 -0700

    Update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,43 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * Where n1 and n2 are the two numbers
- * r is the buffer that the function will use to store the result
- * size_r is the buffer size
- * The function returns a pointer to the result
- * Return: Always 0
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int carry = 0;
-    int sum = 0;
-
-    while (n1[i] != '\0')
-        i++;
-    while (n2[j] != '\0')
-        j++;
-    if (i + 1 > size_r || j + 1 > size_r)
-        return (0);
-    r[i + 1] = '\0';
-    i--;
-    j--;
-    while (i >= 0 || j >= 0)
-    {
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
-        carry = sum / 10;
-        r[k++] = sum % 10 + '0';
-        i--;
-        j--;
-    }
-    if (carry > 0)
-        r[k++] = carry + '0';
-    if (k > size_r)
-        return (0);
-    r[k] = '\0';
-    for (i = 0; i < k / 2; i++)
-    {
-        char tmp = r[i];
-        r[i] = r[k - i - 1];
-        r[k - i - 1] = tmp;
-    }
-    return (r);
+	int i, j, k, l, m, n;
+	for (i = 0; n1[i]; i++);
+	for (j = 0; n2[j]; j++);
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+	n = m;
+	if (i >= 0)
+		n += n1[i] - '0';
+	if (j >= 0)
+		n += n2[j] - '0';
+	if (i < 0 && j < 0 && n == 0)
+{
+	break;
+}
+	m = n / 10;
+	r[k] = n % 10 + '0';
+}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+	return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+{
+	m = r[k];
+	r[k] = r[l];
+	r[l] = m;
+}
+	return (r);
 }
