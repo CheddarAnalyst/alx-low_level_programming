08778d3aeea6622f221a7edd79a73ef6901ae497
CheddarAnalyst <xan404cheddar@gmail.com>
2023-04-18 00:47:51 -0700

    Initialize a variable of struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,34 +1,28 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
-  * init_dog - Initializes a dog structure
-  * @d: A dog structure.
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Return: Nothing
-  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
+struct dog
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for dog structure
  */
-typedef struct dog dog-t;
-void init_dog(struct dog *d,char *name, float age, char *owner);
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _stlen(char *s);
-
 #endif
